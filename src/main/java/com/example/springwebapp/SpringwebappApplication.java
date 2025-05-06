11ea590a6aaf2b062cba343c5212479a80161b62
Mamatha Gorantla <mgxh5@umsystem.edu>
2025-05-06 13:36:47 -0500

    Initial commit

---
+++ src/main/java/com/example/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,11 @@
+package com.example.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringwebappApplication.class, args);
+    }
+}
